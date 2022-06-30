b7aca0546e3b521a4ebd90055a94c312f7ec494b
Danielxcott <changwilliam826@gmail.com>
2022-06-30 13:41:42 +0630

    adding colors on each adjust sliders

--- app.js
+++ app.js
@@ -25,6 +25,43 @@ function randomColor(){
 
         text.innerText = randomColor;
         div.style.backgroundColor = randomColor;
+        checkTextContrast(randomColor,text);
+
+        //grab a slider on each div
+        const colorSlide = div.querySelectorAll(".sliders input");
+        const color = chroma(randomColor);
+        const hue = colorSlide[0];
+        const bright = colorSlide[1];
+        const saturate = colorSlide[2];
+        colorsSlide(color,hue,bright,saturate);
     })
 }
 randomColor();
+
+//check contrast
+function checkTextContrast(color,text){
+    const luminance = chroma(color).luminance();
+    if(luminance>0.5){
+        text.style.color = "black";
+    }else{
+        text.style.color = "white";
+    }
+}
+
+//colorsSlide
+function colorsSlide(color,hue,bright,saturate){
+    //scale for saturation
+    const noSat = color.set('hsl.s',0);
+    const fullSat = color.set('hsl.s',1);
+    const scale = chroma.scale([noSat,color,fullSat,]);
+    //scale for brightness
+    const midBright = color.set('hsl.l',0.5);
+    const scaleBright = chroma.scale(['black',midBright,'white']);
+
+    //sliders
+    saturate.style.backgroundImage = `linear-gradient(to right,${scale(0)},${scale(1)})`;
+
+    bright.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)},${scaleBright(0.5)},${scaleBright(1)})`;
+
+    hue.style.backgroundImage = `linear-gradient(to right,rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
